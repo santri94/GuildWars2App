6c11b50b9241864f6c4f53d0cc429fb803f1ee15
Santiago Rivera <riveras8@montclair.edu>
2019-08-20 09:25:45 -0400

    Fixed Images displaying them by current directory.

--- Gw2/DisplayChar.xaml.cs
+++ Gw2/DisplayChar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,15 +33,16 @@ namespace Gw2
             Name.Text = selected.name;
             RaceProfession.Text = $"{selected.race} - {selected.profession}";
             CharImage.Source = new BitmapImage(new Uri(GetImage()));
-            Backpack.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Backpack.png"));
-            Helm.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Helm.png"));
-            Boots.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Boots.png"));
-            Gloves.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Gloves.png"));
-            Ring1.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Ring.png"));
-            Ring2.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Ring.png"));
-            Amulet.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Amulet.png"));
-            WeaponA1.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Weapon.png"));
-            WeaponA2.Source = new BitmapImage(new Uri("C:\\Gw2 Api\\Gw2\\Images\\Items\\Weapon.png"));
+
+            Backpack.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Backpack.png"));
+            Helm.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Helm.png"));
+            Boots.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Boots.png"));
+            Gloves.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Gloves.png"));
+            Ring1.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Ring.png"));
+            Ring2.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Ring.png"));
+            Amulet.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Amulet.png"));
+            WeaponA1.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Weapon.png"));
+            WeaponA2.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Images\\Items\\Weapon.png"));
 
         }
 
@@ -49,23 +51,23 @@ namespace Gw2
             string image = "";
             if (selected.race == "Asura")
             {
-                image = "C:\\Gw2 Api\\Gw2\\Images\\Asura.jpg";
+                image = Directory.GetCurrentDirectory() + "\\Images\\Asura.jpg";
             }
             if (selected.race == "Charr")
             {
-                image = "C:\\Gw2 Api\\Gw2\\Images\\Charr.jpg";
+                image = Directory.GetCurrentDirectory() + "\\Images\\Charr.jpg";
             }
             if (selected.race == "Human")
             {
-                image = "C:\\Gw2 Api\\Gw2\\Images\\Human.jpg";
+                image = Directory.GetCurrentDirectory() + "\\Images\\Human.jpg";
             }
             if (selected.race == "Norn")
             {
-                image = "C:\\Gw2 Api\\Gw2\\Images\\Norn.jpg";
+                image = Directory.GetCurrentDirectory() + "\\Images\\Norn.jpg";
             }
             if (selected.race == "Sylvari")
             {
-                image = "C:\\Gw2 Api\\Gw2\\Images\\Sylvari.jpg";
+                image = Directory.GetCurrentDirectory() + "\\Images\\Sylvari.jpg";
             }
             return image;
         }
